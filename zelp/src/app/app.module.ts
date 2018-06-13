92f036910faeab92c676ac06ec28e563375e3c25
Abel Shin <shin.ab@husky.neu.edu>
2018-06-12 21:33:51 -0400

    Homepage and redirect added

---
+++ zelp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ProfileComponent,
+    RegisterComponent,
+    HomePageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ zelp/src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ProfileComponent} from './profile/profile.component';
+import {HomePageComponent} from './home-page/home-page.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomePageComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
+  { path: 'profile', component: ProfileComponent}
+];
+export const routing = RouterModule.forRoot(appRoutes);
