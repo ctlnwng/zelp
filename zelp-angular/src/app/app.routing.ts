ee09fcb88fd6407baacb0b1ae8f02743d22cf1f4
Abel Shin <shin.ab@husky.neu.edu>
2018-06-12 21:14:02 -0400

    User login/registration/profile added

--- zelp-angular/src/app/app.routing.ts
+++ zelp-angular/src/app/app.routing.ts
@@ -1,7 +1,7 @@
 import { Routes, RouterModule } from '@angular/router';
-import {LoginComponent} from "./login/login.component";
-import {RegisterComponent} from "./register/register.component";
-import {ProfileComponent} from "./profile/profile.component";
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ProfileComponent} from './profile/profile.component';
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'login', component: LoginComponent},
