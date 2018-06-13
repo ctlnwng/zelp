48dab7a4f1dac851ffc7c30f0532d2aba0527f15
Abel Shin <shin.ab@husky.neu.edu>
2018-06-12 22:03:57 -0400

    ZELP-01 Duplicate zelp folder removed

--- zelp-angular/src/app/app.routing.ts
+++ zelp-angular/src/app/app.routing.ts
@@ -2,10 +2,14 @@ import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {ProfileComponent} from './profile/profile.component';
+import {HomePageComponent} from './home-page/home-page.component';
+
 const appRoutes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomePageComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
-  { path: 'profile', component: ProfileComponent}
+  { path: 'profile', component: ProfileComponent},
+  { path: '**', component: HomePageComponent}
 ];
 export const routing = RouterModule.forRoot(appRoutes);
--- zelp-angular/src/app/register/register.component.ts
+++ zelp-angular/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {UserServiceClient} from '../services/user.service.client';
 
