b10d7b1975d1b82310d01c0d9bff2c86f0f4e068
Abel Shin <shin.ab@husky.neu.edu>
2018-06-15 09:35:57 -0400

    Routing to post page

--- zelp-angular/src/app/post/post.component.ts
+++ zelp-angular/src/app/post/post.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {AlertServiceClient} from '../services/alert.service.client';
+import {Router} from '@angular/router';
+import {Restaurant} from '../models/restaurant.model.client';
+import {PostServiceClient} from '../services/post.service.client';
+import {Post} from '../models/post.model.client';
 
 @Component({
   selector: 'app-post',
@@ -6,10 +11,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  constructor(private service: PostServiceClient, private router: Router, private alertService: AlertServiceClient) {}
 
-  constructor() { }
+  post: Post = new Post();
+
 
   ngOnInit() {
+    this.service.findPostById('5b23bfe75b20fa59e852af95')
+      .then(post => (this.post = post),
+      error => this.alertService.error(error));
   }
-
 }
