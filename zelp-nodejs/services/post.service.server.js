747e17d6e7d7ab8b536a4182bda0f84a74c702b5
Caitlin Wang <caitlinwang97@gmail.com>
2018-06-14 22:49:16 -0400

    fixed look of new post component and changed author to username

--- zelp-nodejs/services/post.service.server.js
+++ zelp-nodejs/services/post.service.server.js
@@ -1,27 +1,33 @@
 module.exports = function(app) {
-    app.get("/api/post", findAllPosts);
-    app.get("/api/post/:pid", findPostById);
-    app.post("/api/post", createPost);
+  app.get("/api/post", findAllPosts);
+  app.get("/api/post/:pid", findPostById);
+  app.post("/api/post", createPost);
 
-    var postModel = require("../models/post/post.model.server");
+  var postModel = require("../models/post/post.model.server");
 
-    function findPostById(req, res) {
-        var id = req.params["pid"];
-        postModel.findPostById(id).then(function(post) {
-            res.json(post);
-        });
-    }
+  function findPostById(req, res) {
+    var id = req.params["pid"];
+    postModel.findPostById(id).then(function(post) {
+      res.json(post);
+    });
+  }
 
-    function createPost(req, res) {
-        var post = req.body;
-        postModel.createPost(post).then(function(post) {
-            res.send(post);
-        });
-    }
+  function createPost(req, res) {
+    var post = {
+      author: req.session["currentUser"].username,
+      title: req.body.title,
+      description: req.body.description,
+      responses: []
+    };
 
-    function findAllPosts(req, res) {
-        postModel.findAllPosts().then(function(posts) {
-            res.send(posts);
-        });
-    }
+    postModel.createPost(post).then(function(post) {
+      res.send(post);
+    });
+  }
+
+  function findAllPosts(req, res) {
+    postModel.findAllPosts().then(function(posts) {
+      res.send(posts);
+    });
+  }
 };
