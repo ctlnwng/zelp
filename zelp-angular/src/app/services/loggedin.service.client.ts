fe0eeb2b3f174d8b25d65cce806b932c213c17db
Abel Shin <shin.ab@husky.neu.edu>
2018-06-22 19:44:41 -0400

    user role setups

--- zelp-angular/src/app/services/loggedin.service.client.ts
+++ zelp-angular/src/app/services/loggedin.service.client.ts
@@ -7,9 +7,16 @@ export class LoggedinServiceClient {
   private messageSource = new BehaviorSubject<boolean>(false || localStorage.getItem('loggedIn') === 'true');
   currentMessage = this.messageSource.asObservable();
 
+  private roleSource = new BehaviorSubject<string>(localStorage.getItem('userRole'));
+  currentUserRole = this.roleSource.asObservable();
+
   constructor() {}
 
   changeMessage(message: boolean) {
     this.messageSource.next(message);
+  }
+
+  changeUserType(message: string) {
+    this.roleSource.next(message);
   }
 }
